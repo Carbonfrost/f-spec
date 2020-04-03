23e8dd66f068fdd9f1ce06efe397649465e515d3
Gary Mann <garythemann@users.noreply.github.com>
2020-04-02 21:13:32 -0700

    Engineering platform update

---
+++ eng/.mk/variables.mk
@@ -0,0 +1,11 @@
+# The configuration to build (probably "Debug" or "Release")
+CONFIGURATION ?= Release
+
+# The framework to publish
+FRAMEWORK ?= netcoreapp3.0
+
+# The location of the NuGet configuration file
+NUGET_CONFIG_FILE ?= ./nuget.config
+
+# Prefix to install
+PREFIX ?= /usr/local
