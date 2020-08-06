090b7daa7ffd269597762425c5ed866a8aedcf4e
Gary Mann <garythemann@users.noreply.github.com>
2020-08-05 23:33:56 -0700

    Add field-property agnostic accessor
    * Improve API by supporting name
    * Add an accessor that supports either field or property

--- dotnet/src/Carbonfrost.Commons.Spec/Src/Carbonfrost/Commons/Spec/ExecutionModel/MemberAccessors.cs
+++ dotnet/src/Carbonfrost.Commons.Spec/Src/Carbonfrost/Commons/Spec/ExecutionModel/MemberAccessors.cs
@@ -1,11 +1,11 @@
 //
-// Copyright 2016 Carbonfrost Systems, Inc. (http://carbonfrost.com)
+// Copyright 2016, 2020 Carbonfrost Systems, Inc. (https://carbonfrost.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,6 @@
 // limitations under the License.
 //
 using System;
-using System.Linq;
 using System.Reflection;
 
 namespace Carbonfrost.Commons.Spec.ExecutionModel {
@@ -29,6 +28,54 @@ namespace Carbonfrost.Commons.Spec.ExecutionModel {
             return new FieldAccessor(field);
         }
 
+        public static IMemberAccessor PropertyOrField(string name) {
+            return new EitherAccessor(name);
+        }
+
+        public static IMemberAccessor PropertyOrField(MemberInfo mem) {
+            if (mem is FieldInfo fi) {
+                return Field(fi);
+            }
+            return Property((PropertyInfo) mem);
+        }
+
+        private static object GetFieldOrProperty(string name, object instance) {
+            var pi = instance.GetType().GetProperty(name);
+            if (pi != null) {
+                return pi.GetValue(instance);
+            }
+            var field = instance.GetType().GetField(name);
+            if (field != null) {
+                return field.GetValue(instance);
+            }
+            return null;
+        }
+
+        struct EitherAccessor : IMemberAccessor {
+            private readonly string _name;
+
+            public EitherAccessor(string name) {
+                _name = name;
+            }
+
+            public object GetValue(object instance) {
+                return GetFieldOrProperty(_name, instance);
+            }
+
+            public string Name {
+                get {
+                    return _name;
+                }
+            }
+
+            public Type ReturnType {
+                get {
+                    return typeof(object);
+                }
+            }
+
+        }
+
         struct PropertyAccessor : IMemberAccessor {
             private readonly PropertyInfo _property;
 
@@ -38,6 +85,12 @@ namespace Carbonfrost.Commons.Spec.ExecutionModel {
 
             public object GetValue(object instance) {
                 return _property.GetValue(instance);
+            }
+
+            public string Name {
+                get {
+                    return _property.Name;
+                }
             }
 
             public Type ReturnType {
@@ -56,6 +109,12 @@ namespace Carbonfrost.Commons.Spec.ExecutionModel {
 
             public object GetValue(object instance) {
                 return _field.GetValue(instance);
+            }
+
+            public string Name {
+                get {
+                    return _field.Name;
+                }
             }
 
             public Type ReturnType {
